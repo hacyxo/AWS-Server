5b63a95daa5a49ac5937578be71773179fad3f1a
hacyxo <sh01020038@gmail.com>
2023-08-18 07:41:40 +0000

    Upload Photo Test

--- myapp/myapp.py
+++ myapp/myapp.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 from pytz import timezone
 from datetime import datetime
+from PIL import Image
 
+import os
 
 app = Flask(__name__)
 
@@ -25,11 +27,28 @@ def sensor_data():
 #Test
 @app.route("/page/now")
 def page_now():
+    #function to display the current time in Korea
     KST = timezone('Asia/Seoul')
     now = datetime.now()
     now = now.astimezone(KST)
     return render_template("page_test.html", now = now)
 
+#Upload Image Test
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_photo():
+    if request.method == "POST":
+        file = request.files["file"]
+        file.save("static/img/" + file.filename)
+        image_path = "static/img/" + file.filename
+        if os.path.exists(image_path):
+            image = Image.open(image_path)
+
+        else:
+            image = "The image dose not exist."
+        return render_template("print_photo.html", image = image)
+
+    else:
+        return render_template("upload_photo.html")
 
 if __name__ == '__main__':
     app.run(debug=False)
